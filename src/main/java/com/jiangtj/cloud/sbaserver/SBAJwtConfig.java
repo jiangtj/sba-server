d121670b5007b83170bb29ec0901f8288eca9057
Mr.J <116749895@qq.com>
2022-03-16 17:49:05 +0800

    init

---
+++ src/main/java/com/jiangtj/cloud/sbaserver/SBAJwtConfig.java
@@ -0,0 +1,26 @@
+package com.jiangtj.cloud.sbaserver;
+
+import com.jiangtj.utils.authspringstarter.AuthServer;
+import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+/**
+ * Created At 2021/3/24.
+ */
+@Configuration
+public class SBAJwtConfig {
+
+    @Bean
+    public HttpHeadersProvider customHttpHeadersProvider(AuthServer authServer) {
+        return instance -> {
+            String instanceName = instance.getRegistration().getName();
+            HttpHeaders httpHeaders = new HttpHeaders();
+            String header = authServer.builder(instanceName).setAudience("*").build();
+            httpHeaders.add(authServer.getOptions(instanceName).getRequest().getHeaderName(), header);
+            return httpHeaders;
+        };
+    }
+
+}
